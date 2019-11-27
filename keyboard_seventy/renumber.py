4e05f66fac5d554db0b824bb5cb3f1c501b29f19
LothTikar <LothTikar@gmail.com>
2019-11-27 16:18:30 -0600

    arranged and numbered diodes

--- keyboard_seventy/renumber.py
+++ keyboard_seventy/renumber.py
@@ -3,10 +3,10 @@ import io
 with open("keyboard_seventy.kicad_pcb", "r") as file:
     data = file.readlines()
     
-count = 121
+count = 120
 for l in range(len(data)):
-    if "SW2" in data[l]:
-        data[l] = data[l].replace("SW2", "SW" + str(count))
+    if " D1 " in data[l]:
+        data[l] = data[l].replace(" D1 ", " D" + str(count) + " ")
         print(data[l])
         count -= 1
         
